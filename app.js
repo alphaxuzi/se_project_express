821b42902ebcfcaf34dc6398aa2a55b5e4745962
Alpha Bah <alphabah2395@gmail.com>
2024-10-29 12:04:59 -0400

    project express

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
+
 app.use(requestLogger);
 app.use('/', mainRouter);
 
