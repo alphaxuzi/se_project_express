946609e0307936fb0646dffdbeeffffe4d698742
Alpha Bah <alphabah2395@gmail.com>
2024-10-31 02:21:11 -0400

    project express

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const {
-  CONFLICT,
   BadRequestError,
   ConflictError,
   UnauthorizedError,
